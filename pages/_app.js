128ffe9aea43bb7a264c52b32030db083f4866ea
arthurjunior <arthur360thug@gmail.com>
2021-07-14 21:58:40 -0400

    Re organizando o projeto e pastas

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import { AlurakutStyles } from '../src/componets/lib/AlurakutCommons';
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
   /* Reset CSS (Necolas Reset CSS <3) */
@@ -8,20 +8,24 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
   body {
     font-family: sans-serif;
     background-color: #D9E6F6;
   }
+
   #__next {
     display: flex;
     min-height: 100vh;
     flex-direction: column;
   }
+
   img {
     max-width: 100%;
     height: auto;
     display: block;
   }
+
   ${AlurakutStyles}
 `
 
@@ -29,4 +33,15 @@ const theme = {
   colors: {
     primary: 'red',
   },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
